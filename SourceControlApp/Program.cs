d8185af2b568b780a7c7208866d51a3a86d55c63
NCarranza-87 <36281270+NCarranza-87@users.noreply.github.com>
2018-07-03 12:13:00 -0700

    Add project files.

---
+++ SourceControlApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceControlApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*******************************************************************************************************
+            For this assignment you will need to create a project and use source control. We will be using GitHub
+
+            You will need to create a project repository for this project.Our goal is to get familiar with checking 
+            in code(commit), and be familiar with the GitHub website portal for your projects. 
+            I recommend you also explore GitHub issues for your project and branching.
+
+            Minimum Requirements:
+
+            You must do at least 3 commits to the project repository(you don't have to create a useful application 
+            for this first assignment. Submit a direct link to your project repository
+            ******************************************************************************************************/
+
+            string name = "";
+            Console.Write("Please enter your name: ");
+            name = Convert.ToString(Console.ReadLine());
+
+
+            Console.WriteLine();
+            Console.Write("My name is " + name);
+            Console.ReadKey();
+        }
+    }
+}
